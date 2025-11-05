ebb66ffd291044cda00307fd1bc93cf296c8bd9f
alanmiguel <alan.rodriguez@navegasoft.com>
2025-11-05 14:14:34 +0100

    odoo19

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
 	"name"         : "Webkul Message Wizard",
 	"summary"      : """To show messages/warnings in Odoo""",
 	"category"     : "Tools",
-	"version"      : "1.0.2",
+	"version"      : "19.0.2",
 	"sequence"     : 1,
 	"author"       : "Webkul Software Pvt. Ltd.",
 	"website"      : "https://store.webkul.com/Odoo.html",
